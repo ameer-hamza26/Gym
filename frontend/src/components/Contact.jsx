9e1edc466cb9eaa8759713de0223d843503fbbcd
Ameer Hamza <169024920+ameer-hamza26@users.noreply.github.com>
2025-05-06 18:30:41 +0500

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "gym-production-7516.up.railway.app/send/mail",
         {
           name,
           email,
